80557c4bd20297bc356ac0604496a533ca4e3ba0
Paiko006 <paikobro006@gmail.com>
2026-03-18 22:06:38 +0700

    update project

--- src/components/canvas-text-demo.tsx
+++ src/components/canvas-text-demo.tsx
@@ -10,11 +10,12 @@ export default function CanvasTextDemo() {
         className="text-4xl font-bold md:text-6xl lg:text-8xl"
         backgroundClassName="bg-black dark:bg-neutral-700"
         colors={[
-          "var(--color-blue-500)",
-          "var(--color-sky-500)",
-          "var(--color-violet-500)",
-          "var(--color-teal-500)",
+          "#3b82f6", // Blue 500
+          "#0ea5e9", // Sky 500
+          "#8b5cf6", // Violet 500
+          "#14b8a6", // Teal 500
         ]}
+
         lineGap={6}
         animationDuration={10}
       />
