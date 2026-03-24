c00f86501ccc685f1df193a1a62c1c6d19271b54
Paiko006 <paikobro006@gmail.com>
2026-03-24 23:06:54 +0700

    update file

--- src/pages/DashboardHome.tsx
+++ src/pages/DashboardHome.tsx
@@ -353,7 +353,7 @@ export default function DashboardHome() {
                       <td className="px-5 py-3.5 text-right">
                         <div className="flex items-center justify-end gap-2">
                           <a 
-                            href={`https://explorer.shelby.xyz/testnet/blob/${b.blob_name}`}
+                            href={`https://explorer.shelby.xyz/testnet/account/${b.owner}`}
                             target="_blank"
                             rel="noopener noreferrer"
                           >
